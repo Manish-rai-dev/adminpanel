46ad65cde6952bb95a9b2232b5e3d2b5556d4b9e
Manish-rai-dev <raimanishkumar52@gmail.com>
2022-06-16 23:48:55 +0530

    widgets part added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import "./home.scss"
+import Widget from "../../components/widget/Widget";
 const home = () => {
   return (
     <div className='home'>
       <Sidebar/>
       <div className="homeContainer">
 <Navbar/>
+<div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   )
